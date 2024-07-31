1d56840486fc15993654618526e96ddb4aef11f3
nikhilsbhat <nikhilsbhat93@gmail.com>
2024-07-31 09:28:31 +0530

    Add brew formula for yamll latest

--- Formula/yamll.rb
+++ Formula/yamll.rb
@@ -5,21 +5,21 @@
 class Yamll < Formula
   desc "A utility that allows importing YAML files as libraries, similar to how programming languages manage dependencies"
   homepage "https://github.com/nikhilsbhat/yamll"
-  version "0.0.3"
+  version "0.0.4"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/nikhilsbhat/yamll/releases/download/v0.0.3/yamll_0.0.3_darwin_x86_64.tar.gz", using: CurlDownloadStrategy
-      sha256 "8ad718b4d959928c5d3f08ec37af95554d2be15a25e184903e036640d7110364"
+      url "https://github.com/nikhilsbhat/yamll/releases/download/v0.0.4/yamll_0.0.4_darwin_x86_64.tar.gz", using: CurlDownloadStrategy
+      sha256 "49220f44be7628ad64767120703e3bf885ca13e7336a2510779251613343be45"
 
       def install
         bin.install "yamll"
       end
     end
     on_arm do
-      url "https://github.com/nikhilsbhat/yamll/releases/download/v0.0.3/yamll_0.0.3_darwin_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "dddc0683c5c80be5628b4e0d9c0d578ccbe7605a2c87343f4017fb68ee90bc4d"
+      url "https://github.com/nikhilsbhat/yamll/releases/download/v0.0.4/yamll_0.0.4_darwin_arm64.tar.gz", using: CurlDownloadStrategy
+      sha256 "7a1b7ca8b315fdcb8ca0a8bca7d8c06254d66a3f5bb591b972e4fa7c94e60935"
 
       def install
         bin.install "yamll"
@@ -30,8 +30,8 @@ class Yamll < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/nikhilsbhat/yamll/releases/download/v0.0.3/yamll_0.0.3_linux_x86_64.tar.gz", using: CurlDownloadStrategy
-        sha256 "7d6a17ceb16cc8d8eb905bc1df2434ba5705e9904c08e63423edf72ef9b59b9d"
+        url "https://github.com/nikhilsbhat/yamll/releases/download/v0.0.4/yamll_0.0.4_linux_x86_64.tar.gz", using: CurlDownloadStrategy
+        sha256 "e5ddfb18e6ab31da1149f88cc549a97347dc83a98db5c6a7a063e5db44110769"
 
         def install
           bin.install "yamll"
@@ -40,8 +40,8 @@ class Yamll < Formula
     end
     on_arm do
       if !Hardware::CPU.is_64_bit?
-        url "https://github.com/nikhilsbhat/yamll/releases/download/v0.0.3/yamll_0.0.3_linux_arm.tar.gz", using: CurlDownloadStrategy
-        sha256 "dda7e6578512485905ec8032d715223dad5551eb5de2ddcb22cb421aa06df955"
+        url "https://github.com/nikhilsbhat/yamll/releases/download/v0.0.4/yamll_0.0.4_linux_arm.tar.gz", using: CurlDownloadStrategy
+        sha256 "00db096e43903fda0cae93129b68bd54d847a449b324ca9193b2a1bf48e20a94"
 
         def install
           bin.install "yamll"
@@ -50,8 +50,8 @@ class Yamll < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/nikhilsbhat/yamll/releases/download/v0.0.3/yamll_0.0.3_linux_arm64.tar.gz", using: CurlDownloadStrategy
-        sha256 "badccbbcf0b39cd39d90abaa41283905b7b8c2bb124fd549d5275b0af9552ad0"
+        url "https://github.com/nikhilsbhat/yamll/releases/download/v0.0.4/yamll_0.0.4_linux_arm64.tar.gz", using: CurlDownloadStrategy
+        sha256 "7cb1aeb2b5c5232b76a5ef57f4e2bde71f56b20d04c30b743b8cd3f3e77ab4f2"
 
         def install
           bin.install "yamll"
